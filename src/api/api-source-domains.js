77bb92016c207df731d7cb12bd91888f009401c1
metlis <andrei.rusov8@gmail.com>
2020-04-25 16:51:52 +0300

    dictionaries modified

--- src/api/api-source-domains.js
+++ src/api/api-source-domains.js
@@ -1,5 +1,5 @@
 const domains = (function () {
-  const d = ['vz.ru', 'tass.ru', 'vesti.ru', 'echo.msk.ru', 'yandex.ru', 'gazeta.ru', 'lenta.ru',
+  const d = ['vz.ru', 'tass.ru', 'vesti.ru', 'yandex.ru', 'gazeta.ru', 'lenta.ru',
     'rbc.ru', 'interfax.ru', 'iz.ru', 'fontanka.ru', 'kommersant.ru', 'mail.ru', 'znak.com',
     'ria.ru', 'news.tut.by', 'rg.ru', 'novayagazeta.ru', 'onliner.by', 'www.mk.ru'];
   return d.join(',');
