b4787c06512fd840efe2bd0822ce529f1896ff17
metlis <andrei.rusov8@gmail.com>
2020-06-30 14:12:51 -0400

    data fetch modidied

--- src/api/api-call-method.js
+++ src/api/api-call-method.js
@@ -1,8 +1,10 @@
+import axios from 'axios';
 import API_ENDPOINT from './api-endpoint';
 import apiCallParams from './api-params';
 
-export default function fetchNews(category = '') {
+export default async function fetchNews(category = '') {
   const paramsStr = apiCallParams.createParamsStr(category);
   const uri = `${API_ENDPOINT}?${paramsStr}`;
-  return fetch(uri).then((response) => response.json());
+  const r = await axios.get(uri);
+  return r.data;
 }
